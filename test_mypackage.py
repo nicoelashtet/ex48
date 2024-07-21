9885ca9a16c59fdfd2e689d1320f332d5228d3f1
pannyumon <pannyum@gmail.com>
2024-07-21 12:21:42 +0900

    Update test_mypackage.py

--- test_mypackage.py
+++ test_mypackage.py
@@ -1,6 +1,6 @@
 import pytest
 from ex48 import WORD_TYPES, convert_number, scan
-def test_word_types():
+def test_word_types():
     assert WORD_TYPES["north"] == "direction"
     assert WORD_TYPES["south"] == "direction"
     assert WORD_TYPES["east"] == "direction"
@@ -13,12 +13,12 @@ from ex48 import WORD_TYPES, convert_number, scan
     assert WORD_TYPES["of"] == "stop"
     assert WORD_TYPES["bear"] == "noun"
     assert WORD_TYPES["princess"] == "noun"
-def test_convert_number():
+def test_convert_number():
     assert convert_number("1234") == 1234
     assert convert_number("3") == 3
     assert convert_number("91234") == 91234
     assert convert_number("abc") is None
-def test_scan():
+def test_scan():
     assert scan("north") == [("direction", "north")]
     assert scan("north south east") == [("direction", "north"), ("direction", "south"), ("direction", "east")]
     assert scan("go kill eat") == [("verb", "go"), ("verb", "kill"), ("verb", "eat")]
